e79360386c811246586077089e712ea1c8c89590
Rubesh Kumar <rubesh.kumar@coupa.com>
2020-01-25 16:40:33 +0530

    changed rails app root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-#get 'orders/new'
- # post 'orders/create'
-  #get 'orders/:id/show', to: 'orders#show'
+
+  root to: 'products#index'
   resources :orders, :only => [:show, :new, :create]
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
