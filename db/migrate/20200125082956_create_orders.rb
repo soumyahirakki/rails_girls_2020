3117781fc4ec4672edd0aad6d18a803c697e44a9
soumya <soumyahirakki555@gmail.com>
2020-01-25 14:06:27 +0530

    added models

--- db/migrate/20200125082956_create_orders.rb
+++ db/migrate/20200125082956_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.decimal :total_price
-      t.string :order_id
+      t.string :order_identifier
       t.timestamps
     end
   end
